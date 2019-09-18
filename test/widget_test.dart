50c5d988324afcb1b1aac203e52a30e23e3c5a2e
Todd Burgess <tburgess57@gmail.com>
2019-09-18 18:22:17 -0400

    Remove counter code from main screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cat_adopt_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TCRPetList());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
