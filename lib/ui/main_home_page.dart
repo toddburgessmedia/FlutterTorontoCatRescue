f9e90a9b5ab8652e4ae392e6e5242e14ca226848
Todd Burgess <tburgess57@gmail.com>
2019-09-22 11:10:06 -0400

    Restructure the app UI

---
+++ lib/ui/main_home_page.dart
@@ -0,0 +1,51 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:cat_adopt_flutter/bloc/pet_list_bloc.dart';
+import 'package:cat_adopt_flutter/model/pet_list.dart';
+import 'package:flutter/material.dart';
+
+class MainHomePage extends StatefulWidget {
+  MainHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MainHomePageState createState() => _MainHomePageState();
+}
+
+class _MainHomePageState extends State<MainHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    petListBloc.updatePetList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: StreamBuilder(
+            stream: petListBloc.petListStream,
+            builder: (context, AsyncSnapshot<PetList> snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data.petList.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: Image.network(snapshot.data.petList[index].largePhotoURL),
+                      title: Text(snapshot.data.petList[index].petName),
+                      subtitle: Text(snapshot.data.petList[index].primaryBreed),
+                      isThreeLine: true,
+                    );
+                  },
+                );
+              } else if (snapshot.hasError){
+                return Text ('Error');
+              }
+              return Center(child: CircularProgressIndicator());
+            }));
+  }
+}
