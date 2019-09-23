38475ea782f22021e913e7e4dc1a577e2e8509f3
Todd Burgess <tburgess57@gmail.com>
2019-09-23 12:03:44 -0400

    Add PetDetail photo viewer

---
+++ lib/ui/petdetail/pet_detail_photo.dart
@@ -0,0 +1,52 @@
+
+import 'package:cat_adopt_flutter/model/pet_detail_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PetDetailPhoto extends StatefulWidget {
+
+  final List<PetDetailImage> photos;
+
+  PetDetailPhoto({Key, key,this.photos}) : super();
+
+  @override
+  _PetDetailPhotoState createState() => _PetDetailPhotoState();
+
+}
+
+class _PetDetailPhotoState extends State<PetDetailPhoto> {
+
+  String displayphoto;
+
+
+  @override
+  void initState() {
+    displayphoto = widget.photos[0].originalUrl;
+  }
+
+
+  void _changeImage(String newPhoto) {
+    setState(() {
+      displayphoto = newPhoto;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column (
+        children: <Widget>[
+          Image.network(displayphoto),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: widget.photos.map((photo) =>
+                  GestureDetector(
+                      onTap: () => _changeImage(photo.originalUrl) ,
+                      child: Image.network(photo.thumbnailUrl))
+              ).toList()
+          ),
+        ],
+    );
+  }
+
+
+}
--- lib/ui/petdetail/pet_detail_view.dart
+++ lib/ui/petdetail/pet_detail_view.dart
@@ -1,6 +1,9 @@
 
 import 'package:cat_adopt_flutter/model/pet_detail_info.dart';
+import 'package:cat_adopt_flutter/ui/petdetail/pet_detail_photo.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_html_view/flutter_html_view.dart';
 
 class PetDetailView extends StatelessWidget {
 
@@ -10,8 +13,18 @@ class PetDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Meow. My name is ${petDetail.petName}"));
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text("Meow. My name is ${petDetail.petName}", textScaleFactor: 2,),
+          PetDetailPhoto(photos: petDetail.petImages),
+          Expanded(child: Text(petDetail.description))
+        ],
+      ),
+    );
+
   }
 
 
 }
+
