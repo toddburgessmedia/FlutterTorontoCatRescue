fbc0ef10a5257619426250d8405e2b011a475df4
Todd Burgess <tburgess57@gmail.com>
2019-10-13 14:54:27 -0400

    Add photo carosel to pet detail page

--- lib/ui/petdetail/pet_detail_photo.dart
+++ lib/ui/petdetail/pet_detail_photo.dart
@@ -1,4 +1,5 @@
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cat_adopt_flutter/model/pet_detail_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -16,44 +17,32 @@ class PetDetailPhoto extends StatefulWidget {
 
 class _PetDetailPhotoState extends State<PetDetailPhoto> {
 
-  String displayphoto;
   String placeHolder = 'images/tcr-placeholder.png';
-
-
-  @override
-  void initState() {
-    super.initState();
-    displayphoto = widget.photos[0].originalUrl;
-  }
-
-
-  void _changeImage(String newPhoto) {
-    setState(() {
-      placeHolder = displayphoto;
-      displayphoto = newPhoto;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Column (
-        children: <Widget>[
-          FadeInImage.assetNetwork(placeholder: placeHolder , image: displayphoto,height: 350, ),
-            Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: widget.photos.map((photo) =>
-                  Flexible(
-                    flex: 1,
-                    child: InkWell(
-                        onTap: () => _changeImage(photo.originalUrl) ,
-                        child:
-                          FadeInImage.assetNetwork(placeholder: placeHolder , image: photo.thumbnailUrl,height: 100, )
-                  )
-
-                  )).toList()
-          ),
-        ],
-    );
+    if (widget.photos.length == 1) {
+      return FadeInImage.assetNetwork(
+        placeholder: placeHolder, image: widget.photos[0].originalUrl, height: 350,);
+    } else {
+      return
+        CarouselSlider(
+          height: 350,
+          autoPlay: true,
+          pauseAutoPlayOnTouch: Duration(seconds: 60),
+          autoPlayInterval: Duration(seconds: 4),
+          enlargeCenterPage: true,
+          autoPlayCurve: Curves.easeInSine,
+          autoPlayAnimationDuration: Duration(milliseconds: 750),
+          items: widget.photos.map((image) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Image.network(image.originalUrl);
+              },
+            );
+          }).toList(),
+        );
+    }
   }
 
 
