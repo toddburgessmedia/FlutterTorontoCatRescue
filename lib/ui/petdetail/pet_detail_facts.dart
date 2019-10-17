7ead8417db50e32cae01fa2d13e5b485c91dc6cf
Todd Burgess <tburgess57@gmail.com>
2019-10-16 20:24:39 -0400

    Facts about cat information

--- lib/ui/petdetail/pet_detail_facts.dart
+++ lib/ui/petdetail/pet_detail_facts.dart
@@ -2,39 +2,75 @@ import 'package:cat_adopt_flutter/model/pet_detail.dart';
 import 'package:cat_adopt_flutter/model/pet_detail_info.dart';
 import 'package:flutter/material.dart';
 
-class PetDetailFacts extends StatelessWidget {
+class PetDetailFacts extends StatefulWidget {
 
   final PetDetailInfo petDetail;
-  int rows = 1;
 
   PetDetailFacts({Key key, this.petDetail}) : super();
 
   @override
+  _PetDetailFactsState createState() => _PetDetailFactsState();
+}
+
+class _PetDetailFactsState extends State<PetDetailFacts> {
+  int rows = 1;
+
+  int _setGridRows() {
+
+
+    if (MediaQuery.of(context).orientation == Orientation.landscape)
+      return 2;
+    else
+      return 1;
+
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return OrientationBuilder(
-        builder: (context,orientation) {
-      if (orientation == Orientation.landscape)
-        rows = 2;
-      else
-        rows = 1;
       return Column(
         children: <Widget>[
-          Text('Facts About ${petDetail.petName}',style: TextStyle(fontWeight: FontWeight.bold),),
+          Text('Facts About ${widget.petDetail.petName}',style: TextStyle(fontWeight: FontWeight.bold),),
           GridView.count(
             shrinkWrap: true,
-              crossAxisCount: rows,
+              crossAxisCount: _setGridRows(),
               childAspectRatio: 25,
               children: <Widget>[
-                GridTile(child: Text('Breed: ${petDetail.primaryBreed}')),
-                GridTile(child: Text('Colour: ${petDetail.furColour}')),
-                GridTile(child: Text('Age: ${petDetail.age}')),
-                GridTile(child: Text('Sex: ${petDetail.sex}')),
-                GridTile(child: Text('Hair: ${petDetail.hairLength}'))
+                GridTile(
+                    child: RichText(
+                        text: TextSpan(
+                          text: 'Breed: ',
+                          style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),
+                          children: <TextSpan>[
+                            TextSpan(text: widget.petDetail.primaryBreed, style: TextStyle(fontWeight: FontWeight.normal)),
+                  ]),),),
+                GridTile(
+                  child: RichText(
+                    text: TextSpan(
+                        text: 'Colour: ',
+                        style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(text: widget.petDetail.furColour, style: TextStyle(fontWeight: FontWeight.normal)),
+                        ]),),),
+                GridTile(
+                  child: RichText(
+                    text: TextSpan(
+                        text: 'Gender: ',
+                        style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(text: widget.petDetail.sex, style: TextStyle(fontWeight: FontWeight.normal)),
+                        ]),),),
+                GridTile(
+                  child: RichText(
+                    text: TextSpan(
+                        text: 'Hair: ',
+                        style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(text: widget.petDetail.hairLength, style: TextStyle(fontWeight: FontWeight.normal)),
+                        ]),),),
               ],
 
           ),
         ],
-      );},
-    );
+      );
   }
 }
